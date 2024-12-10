afce41cf12584264ef61ba754619209286a8b0e8
llcc01 <2417224420@qq.com>
2024-12-10 22:03:45 +0800

    update 2024

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ cpu-world-scraper
 =================
 
 a webscraper that collects CPU infos into a csv file from the cpu-world.com page
+
+## 2024-12-10
+
+Update the parser for the new page and use Selenium to fetch the required data.
--- scraper.py
+++ scraper.py
@@ -1,211 +1,245 @@
 #!/usr/bin/env python
-#-*- coding:UTF-8 -*-
+# -*- coding:UTF-8 -*-
 
-#rikujjs
+# rikujjs
 
 
+import pickle
 from bs4 import BeautifulSoup
-import requests
 from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-#column_headers array contains the names for the columns.
-column_headers = ["Brand", "Model", "Release", "Clockrate", "Cores", "Cache","FSB", "Socket"]
+# column_headers array contains the names for the columns.
+column_headers = [
+    "Brand",
+    "Model",
+    "Type",
+    "Release",
+    "Clockrate",
+    "Cores",
+    "Cache",
+    "Socket",
+    "URL",
+]
 
 
-months_dict = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
+months_dict = {
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12,
+}
 
+page_cache = {}
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+service = Service("/home/lc/tools/bin/chromedriver")
+
+driver = None
 
 class Scraper:
 
-    #make the parsed soup in to a variable called "soup"
+    # make the parsed soup in to a variable called "soup"
     def __init__(self, url, year, output):
+        global driver
 
         self.url = url
         self.year = year
         self.write_handle = output
 
-        #initialize the datastorage[] dict
+        # member variables
+        self.soup = None
+        self.datastorage = {}
+        self.page_data = []
+
+        # initialize the datastorage[] dict
         for header in column_headers:
-            self.datastorage[header] = '-'
+            self.datastorage[header] = "-"
 
-        #include possible spoofed header, and get the page
-        header = {'User-Agent': 'Mozilla/5.0'}
-        r = requests.get(url, headers=header)
+        if url in page_cache:
+            print("Using cache", url)
+        else:
+            # include possible spoofed header, and get the page
+            if driver is None:
+                driver = webdriver.Chrome(service=service, options=chrome_options)
+            driver.get(url)
+            html_text = driver.page_source
 
-        #make some soup
-        self.soup = BeautifulSoup(r.text.encode('utf-8', 'ignore'))
+            page_cache[url] = html_text
 
-    #we go thru the datastorage[] dict and write the info to a string, separated with ';'
+            # be nice
+            sleep(1)
+
+        self.soup = BeautifulSoup(page_cache[url], "html.parser")
+
+    # we go thru the datastorage[] dict and write the info to a string, separated with ','
     def info_to_string(self):
 
         info_string = ""
 
-        #write the columns to a single string, place a ';' in between
+        # write the columns to a single string, place a ',' in between
         for header in column_headers:
 
-            info_string += ';'
-            info_string += self.datastorage[header].strip().encode('utf-8', 'ignore').replace(";", '')
+            info_string += ","
+            info_string += self.datastorage[header].strip().replace(",", "")
 
-
-        #get rid of the first ;
-        info_string = info_string.replace(";", "", 1)
+        # get rid of the first ,
+        info_string = info_string.replace(",", "", 1)
 
         return info_string
 
-    #format the date to a more usable form, eg. 2-2005
+    # format the date to a more usable form, eg. 2-2005
     def get_date(self, month):
 
         try:
             month_number = months_dict[month]
         except KeyError:
-            print self.url
+            print(self.url)
             return self.year
 
-        return str(month_number)+'-'+self.year
+        return str(self.year) + "-" + str(month_number)
 
-    def get_months_models(self, data, brand, date):
-
+    def get_months_models(self, data, brand, date, type="Unknown"):
 
         self.datastorage["Brand"] = brand
         self.datastorage["Release"] = self.get_date(date)
 
-        models = data.findAll('a')
-        specs = data.findAll("div", "pad1 even_smaller")
+        models = data.findAll("a")
+        specs = data.findAll("div", "rel_sp")
 
-        #remove the first one since theres a extra element which doesnt contain anything useful
-        if brand == "AMD":
-            del models[0]
+        assert len(models) == len(
+            specs
+        ), f"Models and specs length mismatch: {len(models)} != {len(specs)}"
 
-
-        for x in range(0, 50):
-
+        for model, spec_ in zip(models, specs):
 
             try:
 
-                #make sure the dict is empty
+                # make sure the dict is empty
                 for key in self.datastorage:
-                    self.datastorage[key] = '-'
+                    self.datastorage[key] = "-"
 
-                #brand and release
+                # brand and release
                 self.datastorage["Brand"] = brand
                 self.datastorage["Release"] = self.get_date(date)
 
+                self.datastorage["Model"] = model.text
+                self.datastorage["URL"] = "https://www.cpu-world.com" + model["href"]
+                self.datastorage["Type"] = type
 
-                self.datastorage["Model"] = models[x].text
-                specs_split = specs[x].text.split('/')
+                specs_split = spec_.text.split("/")
 
                 for spec in specs_split:
 
                     if "Hz" in spec:
                         self.datastorage["Clockrate"] = spec.strip()
                     elif "core" in spec.lower():
-                        self.datastorage["Cores"] = spec.strip()
+                        self.datastorage["Cores"] = spec.replace("cores", "").replace("core", "").replace(" ", "").strip()
                     elif "socket" in spec.lower():
                         self.datastorage["Socket"] = spec.strip()
                     elif "fsb" in spec.lower():
                         self.datastorage["FSB"] = spec.strip()
-                    elif "l1" in spec.lower() or "l2" in spec.lower() or "l3" in spec.lower() or "l4" in spec.lower():
+                    elif (
+                        "l1" in spec.lower()
+                        or "l2" in spec.lower()
+                        or "l3" in spec.lower()
+                        or "l4" in spec.lower()
+                    ):
                         self.datastorage["Cache"] = spec.strip()
-                    else:
-                        print spec
-                        print self.url
+                    else: 
+                        pass
+                        # print(spec)
+                        # print(self.url)
 
-
-                #place the model info into a string, and store in a array for later use
+                # place the model info into a string, and store in a array for later use
                 model_specs = self.info_to_string()
                 self.page_data.append(model_specs)
 
-
-                #self.write_handle(model_specs)
-                #self.write_handle('\n')
+                # self.write_handle(model_specs)
+                # self.write_handle('\n')
             except IndexError:
                 pass
 
+    # do the actual scraping
+    def get_pageinfo(self, type="Unknown"):
 
-    #do the actual scraping
-    def get_pageinfo(self):
-
-        #do the actual scraping here
+        # do the actual scraping here
         info = self.soup.find("div", "p_div")
-        monthly_info = info.findAll("tr")
+        monthly_info = info.findAll("div", "rel_data")
 
         for month_info in monthly_info:
-            info_line = month_info.findAll('td')
+            info_line = month_info.findAll("div")
 
-            if month_info.find("th") is not None:
+            date = info_line[0].text
+
+            if date is None:
                 continue
 
-            if len(info_line) > 0 and info_line[0] is not None:
+            data_div = info_line[1].findAll("div")
 
-                try:
-                    self.get_months_models(info_line[1], "AMD", info_line[0].text)
-                except:
-                    #this month didnt have amd launches
-                    print "no amd this month"
-                try:
-                    self.get_months_models(info_line[2], "Intel", info_line[0].text)
-                except:
-                    print "no intels"
+            if len(data_div) > 0:
+                self.get_months_models(data_div[0], "AMD", date, type)
+                self.get_months_models(data_div[1], "Intel", date, type)
 
-
-        #eventualy return the collected data
+        # # eventualy return the collected data
         return self.page_data
 
-    #member variables
-    soup = None
-    datastorage = {}
-    year = None
-    url = None
-    write_handle = None
-    page_data = []
 
 if __name__ == "__main__":
 
+    # load cache
+    try:
+        page_cache = pickle.load(open("page_cache.p", "rb"))
+    except:
+        print("No cache found")
+
+    # modifie the starting id if you need the restart the program from a different location. eg. after a crash
+    start_year = 2015
+    years = 10
+
+    type_list = ["Desktop", "Embedded", "Mobile", "Server"]
+
+    # the file we are gonna write the gotten info, the file has the starting_id in it, in case starting
+    # from somewhere else than the begining.
+    output_file = "cpu-world_{0}_{1}.csv".format(start_year, start_year + years - 1)
+    output = open(output_file, "w")
+
+    # write header
+    output.write(",".join(column_headers))
+    output.write("\n")
 
 
-    #modifie the starting id if you need the restart the program from a different location. eg. after a crash
-    starting_id = '2004'
+    for type in type_list:
+        # loop the games from starting_id to end.
+        for year in range(start_year, start_year + years):
 
+            baseurl = "http://www.cpu-world.com/Releases/{type}_CPU_releases_({id}).html".format(
+                id=year, type=type
+            )
 
-    #insert the base of the url's we're going to scrape
-    baseurl = "http://www.cpu-world.com/Releases/Desktop_CPU_releases_({0}).html".format(starting_id)
+            print(baseurl)
 
+            # get the page, BS it, and get the pageinfo
+            page_to_get = Scraper(baseurl, year, output)
+            page_info = page_to_get.get_pageinfo(type)
 
-    #the file we are gonna write the gotten info, the file has the starting_id in it, in case starting
-    #from somewhere else than the begining.
-    output_file = "cpu-world{0}.csv".format(starting_id)
-    output = open(output_file, 'w')
+            for page in page_info:
+                output.write(page)
+                output.write("\n")
 
-    #loop the games from starting_id to end.
-    for i in range(0, 11):
+    # close the file
+    output.close()
 
-        baseurl = "http://www.cpu-world.com/Releases/Desktop_CPU_releases_({0}).html".format(starting_id)
-
-        print baseurl
-
-
-        #get the page, BS it, and get the pageinfo
-        page_to_get = Scraper(baseurl, starting_id, output)
-        page_info = page_to_get.get_pageinfo()
-
-        for page in page_info:
-            output.write(page)
-            output.write('\n')
-
-        #be nice
-        sleep(3)
-
-        next_year = int(starting_id)+1
-        starting_id = str(next_year)
-
-
-
-
-
-
-
-
-
-
-
-
+    # save the cache
+    pickle.dump(page_cache, open("page_cache.p", "wb"))
